9586599187c0e1b4795ae586b88a639b0318e694
Jtremback <hi@jehan.email>
2021-03-15 17:08:23 -0700

    get indexer working for real

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -1,8 +1,11 @@
-import fetch from "node-fetch";
+var http = require("http");
+import fetch from "make-fetch-happen";
 import { ethers } from "ethers";
 import fs from "fs";
 import AWS from "aws-sdk";
 import { DeepGems } from "../../solidity/typechain/DeepGems";
+import { createNotEmittedStatement } from "typescript";
+import { ConfigurationServicePlaceholders } from "aws-sdk/lib/config_service_placeholders";
 const gemArtifact = require("../../solidity/artifacts/contracts/DeepGems.sol/DeepGems.json");
 
 const {
@@ -21,6 +24,7 @@ const {
   AWS_SECRET_ACCESS_KEY,
   INFURA_KEY,
   GEMS_CONTRACT,
+  GRAPHQL_URL,
 }: {
   S3_JSON_CONTEXT_URL: string;
   BLOCKS_PER_FETCH: string;
@@ -37,6 +41,7 @@ const {
   AWS_SECRET_ACCESS_KEY: string;
   INFURA_KEY: string;
   GEMS_CONTRACT: string;
+  GRAPHQL_URL: string;
 } = process.env as any;
 
 function loop() {
@@ -50,7 +55,8 @@ function loop() {
   }, parseInt(LOOP_TIME, 10));
 }
 
-const provider = new ethers.providers.InfuraProvider("rinkeby", INFURA_KEY);
+// const provider = new ethers.providers.InfuraProvider("rinkeby", INFURA_KEY);
+const provider = new ethers.providers.JsonRpcProvider();
 
 const gems = new ethers.Contract(
   GEMS_CONTRACT,
@@ -75,250 +81,122 @@ interface UserMapping {
 
 interface Context {
   lastBlockRetrieved: number;
-  userIndex: string[];
+  // userIndex: string[];
+}
+
+const query = `query GetGems($fromBlock: Int!, $toBlock: Int!) {
+  gems(where: {
+    forgeBlock_gte: $fromBlock,
+		forgeBlock_lte: $toBlock
+  }){
+    id
+    psi
+    owner
+    burned
+    forgeTime
+    forgeBlock
+  }
+}`;
+
+async function getForgedGems(fromBlock: number, toBlock: number) {
+  return (
+    await (
+      await fetch(GRAPHQL_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query, variables: { fromBlock, toBlock } }),
+      })
+    ).json()
+  ).data.gems;
 }
 
 async function run() {
   const rawContext = await (await fetch(S3_JSON_CONTEXT_URL)).json();
   const context: Context = {
     lastBlockRetrieved: parseInt(rawContext.lastBlockRetrieved, 10),
-    userIndex: rawContext.userIndex,
   };
 
-  // Populate users mapping
-  const userMapping: UserMapping = {};
-  for (const userAddr of context.userIndex) {
-    userMapping[userAddr] = await getUserGems(userAddr);
+  const currentBlock = await provider.getBlockNumber();
+
+  if (context.lastBlockRetrieved > currentBlock) {
+    throw new Error("Stored last block retrieved is higher than current block");
   }
 
-  console.log(
-    `Getting events from ${context.lastBlockRetrieved} to ${
-      context.lastBlockRetrieved + parseInt(BLOCKS_PER_FETCH, 10)
-    }`
-  );
-
-  console.log("gems.address", gems.address);
-
-  const currentBlock = 42069;
-
-  const highestBlockToGet = Math.max(
+  const highestBlockToGet = Math.min(
     currentBlock,
     context.lastBlockRetrieved + parseInt(BLOCKS_PER_FETCH, 10)
   );
 
-  // Get events
-  const events = await gems.queryFilter(
-    {
-      address: gems.address,
-      topics: [
-        FORGED_EVENT_TOPIC,
-        REFORGED_EVENT_TOPIC,
-        BURNED_EVENT_TOPIC,
-        ACTIVATED_EVENT_TOPIC,
-        // TRANSFERED_EVENT_TOPIC,
-      ],
-    },
-    context.lastBlockRetrieved,
+  const gems = await getForgedGems(
+    context.lastBlockRetrieved + 1,
     highestBlockToGet
   );
 
-  console.log(`Got ${events.length} events`);
-  console.log(`Events: ${JSON.stringify(events)}`);
+  console.log(
+    "lastBlockRetrieved",
+    context.lastBlockRetrieved,
+    "currentBlock",
+    currentBlock,
+    "highestBlockToGet",
+    highestBlockToGet
+  );
 
-  // Index events
-  const highestBlockInEvents = await indexEvents(context, userMapping, events);
+  console.log(`Got ${gems.length} events`);
+  console.log(`Gems: ${JSON.stringify(gems)}`);
 
-  // Save state
-  // Upload each user
-  for (const userAddr of Object.keys(userMapping)) {
-    uploadToS3(
-      Buffer.from(JSON.stringify(userMapping[userAddr])),
-      S3_DATA_BUCKET,
-      `${userAddr}.json`
-    );
+  for (const gem of gems) {
+    console.log("wtf");
+    await renderGem(gem.id);
   }
 
   // Upload context
-  context.lastBlockRetrieved = highestBlock;
-  context.userIndex = Object.keys(userMapping);
+  context.lastBlockRetrieved = 0; //highestBlockToGet;
 
   uploadToS3(
     Buffer.from(JSON.stringify(context)),
     S3_DATA_BUCKET,
-    `context.json`
+    `context.json`,
+    "application/json"
   );
-}
-
-async function indexEvents(
-  context: Context,
-  userMapping: UserMapping,
-  events: ethers.Event[]
-) {
-  let highestBlock = 0;
-  for (const event of events) {
-    if (event.blockNumber > highestBlock) {
-      highestBlock = event.blockNumber;
-    }
-
-    switch (event.event) {
-      case "Forged":
-        await indexForgedEvent(context, userMapping, event);
-        break;
-
-      case "Reforged":
-        await indexReforgedEvent(context, userMapping, event);
-        break;
-
-      case "Activated":
-        await indexActivatedEvent(context, userMapping, event);
-        break;
-
-      case "Burned":
-        await indexBurnedEvent(context, userMapping, event);
-        break;
-
-      case "Transfer":
-        await indexTransferEvent(context, userMapping, event);
-        break;
-
-      default:
-        throw new Error("Unknown event");
-    }
-  }
-  return highestBlock;
-}
-
-async function indexForgedEvent(
-  context: Context,
-  userMapping: UserMapping,
-  event: ethers.Event
-) {
-  const tokenId = event.args!._id;
-  const owner = event.args!._owner;
-
-  // It is impossible for a gem to already exist
-  if (userMapping[owner].gems[tokenId]) {
-    throw new Error("Found exisiting gem during forged event index");
-  }
-
-  if (!userMapping[owner]) {
-    // Update user entry
-    userMapping[owner] = { gems: {} };
-  }
-
-  userMapping[owner].gems[tokenId] = { activated: false };
-  await renderGem(tokenId);
-}
-
-async function indexReforgedEvent(
-  context: Context,
-  userMapping: UserMapping,
-  event: ethers.Event
-) {
-  const owner = event.args!.owner;
-  const oldTokenId = event.args!.oldTokenId;
-  const newTokenId = event.args!.newTokenId;
-
-  // It is impossible for the old gem not to exist
-  if (!userMapping[owner].gems[oldTokenId]) {
-    throw new Error(
-      "Did not find existing old gem during reforged event index"
-    );
-  }
-
-  // It is impossible for the new gem to already exist
-  if (userMapping[owner].gems[newTokenId]) {
-    throw new Error("Found exisiting new gem during reforged event index");
-  }
-
-  // Update user entry
-  delete userMapping[owner].gems[oldTokenId];
-  userMapping[owner].gems[newTokenId] = { activated: false };
-  await renderGem(newTokenId);
 }
 
 async function renderGem(tokenId: string) {
   // Get metadata
-  const metdata = await gems.getGemMetadata(tokenId);
-  // Upload image to s3
+  let metadata = await gems.getGemMetadata(tokenId);
+
+  console.log("METADATA", metadata);
+  // Render gem
   const img = await (
-    await fetch(`${RENDERER_URL}/${JSON.stringify(metdata)}`)
+    await fetch(
+      `${RENDERER_URL}/${JSON.stringify([
+        metadata[0],
+        metadata[1],
+        metadata[2],
+        metadata[3],
+        metadata[4] / 100,
+      ])}`
+    )
   ).buffer();
 
-  uploadToS3(img, S3_IMAGE_BUCKET, `${tokenId}.jpg`);
+  // Upload image to s3
+  uploadToS3(img, S3_IMAGE_BUCKET, `${tokenId}.jpg`, "image/jpeg");
 }
 
-async function indexActivatedEvent(
-  context: Context,
-  userMapping: UserMapping,
-  event: ethers.Event
+function uploadToS3(
+  data: Buffer,
+  bucket: string,
+  key: string,
+  contentType: string
 ) {
-  const tokenId = event.args!._id;
-  const owner = event.args!._owner;
-
-  // It is impossible for the unactivated gem not to exist
-  if (!userMapping[owner].gems[tokenId]) {
-    throw new Error("Did not find existing gem during activated event index");
-  }
-
-  // It is impossible for the unactivated gem to be activated
-  if (userMapping[owner].gems[tokenId].activated) {
-    throw new Error("Gem was already activated during activated event index");
-  }
-
-  // Update user entry
-  userMapping[owner].gems[tokenId].activated = true;
-}
-
-async function indexTransferEvent(
-  context: Context,
-  userMapping: UserMapping,
-  event: ethers.Event
-) {
-  const from = event.args!.from;
-  const to = event.args!.to;
-  const tokenId = event.args!.tokenId;
-
-  // Gem must exist
-  if (!userMapping[from].gems[tokenId]) {
-    throw new Error("Gem did not exist during transfer event index");
-  }
-
-  // Gem must be activated
-  if (!userMapping[from].gems[tokenId].activated) {
-    throw new Error("Gem was not activated during transfer event index");
-  }
-
-  // Update user entries
-  userMapping[to].gems[tokenId] = userMapping[from].gems[tokenId];
-  delete userMapping[from].gems[tokenId];
-}
-
-async function indexBurnedEvent(
-  context: Context,
-  userMapping: UserMapping,
-  event: ethers.Event
-) {
-  const owner = event.args!.owner;
-  const tokenId = event.args!.tokenId;
-
-  // Update user entry
-  delete userMapping[owner].gems[tokenId];
-}
-
-function getUserGems(addr: string) {
-  return { gems: { foo: { activated: true } } };
-}
-
-function uploadToS3(data: Buffer, bucket: string, key: string) {
-  // Read content from the file
-  // const fileContent = fs.readFileSync("f");
-
   // Setting up S3 upload parameters
-  const params = {
+  const params: AWS.S3.PutObjectRequest = {
     Body: data,
     Bucket: bucket,
     Key: key, // File name you want to save as in S3
+    ContentType: contentType,
   };
 
   // Uploading files to the bucket
